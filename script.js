19e192ae48fc65505143e5d807c92b790092674c
nh110lab-ai <nh110lab.ai@gmail.com>
2025-11-16 17:30:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -153,3 +153,11 @@ document.querySelectorAll("nav a").forEach((link) => {
     });
   });
 });
+document.querySelectorAll("nav a").forEach(link => {
+    link.addEventListener("click", e => {
+        e.preventDefault();
+        const target = document.querySelector(link.getAttribute("href"));
+        target.scrollIntoView({ behavior: "smooth" });
+    });
+});
+
